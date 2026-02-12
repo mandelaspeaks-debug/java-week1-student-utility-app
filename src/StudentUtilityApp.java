bb8b5037c50edae210bce5746686b792f828bae6
mandelaspeaks-debug <mandelaspeaks@gmail.com>
2026-02-12 16:53:40 +0100

    the final work is clean and good

--- src/StudentUtilityApp.java
+++ src/StudentUtilityApp.java
@@ -13,12 +13,14 @@ public class StudentUtilityApp {
             System.out.println("Pick a number from 1 - 5: ");
             choice = scanner.nextInt();
 
+            // had to use the short version of the switch statement I learned while researching on how to complete the project
+
             switch (choice) {
                 case 1 -> addTwoNumbers(scanner);
                 case 2 -> checkOddOrEven(scanner);
                 case 3 -> printNumbers(scanner);
-                case 4 -> System.out.println("the fourth");
-                case 5 -> System.out.println("Exit");
+                case 4 -> displayDayOfTheWeek(scanner);
+                case 5 -> closeApp();
                 default -> System.out.println("LAST LAST");
             }
 
@@ -33,7 +35,7 @@ public class StudentUtilityApp {
     // the methods I will be using
 
     public static void addTwoNumbers(Scanner scanner) {
-        System.out.println("\n Pick two numbers");
+        System.out.println("Pick two numbers");
 
         System.out.println("Enter first Number: ");
         int number1 = scanner.nextInt();
@@ -47,19 +49,48 @@ public class StudentUtilityApp {
 
 
     }
-
+// this method is to check if the number the user inputs is even or odd
     public static void checkOddOrEven (Scanner scanner) {
         System.out.println("Enter a number");
         int numba = scanner.nextInt();
         System.out.println((numba % 2 == 0)? "EVEN" : "ODD");
     }
 
+    // Learning about the scanner method, I realized that i cab create different OBJECT of the scanner
+
     public static void printNumbers (Scanner scanner) {
         System.out.println("Pick a random number: ");
         int printer = scanner.nextInt();
+        if (printer == 0) {
+            System.out.println("Give me a real number not zero!");
+        }
         for (int i = 1; i <= printer; i++) {
             System.out.println(i);
         }
+    }
+// this method is to display the days of the week
+    public static void displayDayOfTheWeek (Scanner scanner) {
+        System.out.println("Pick a number between 1 and 7 to select a day");
+
+        int day = scanner.nextInt();
+
+        switch (day) {
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+            default -> System.out.println("Invalid day");
+        }
+
+
+    }
+    // this close app method could have been neglected, but I had to for readability and ease of us
+    public static void closeApp () {
+        System.out.println("Thank you for using this app");
+
     }
 
 }
